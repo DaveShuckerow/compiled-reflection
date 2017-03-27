6ef9930e52332fa3a5331bf58c2fc4eb4354f45a
Dave Shuckerow <djshuckerow@google.com>
2017-03-27 11:44:02 -0700

    Cleanup imports, docs in codegen and pubspec.

--- tool/codegen.dart
+++ tool/codegen.dart
@@ -3,11 +3,11 @@ import 'dart:async';
 import 'package:build_runner/build_runner.dart';
 import 'package:compiled_mirrors/transformer.dart';
 
+/// Generates compiled mirrors for the annotated classes in the current package.
 Future main() async {
-  /// Builds a full package dependency graph for the current package.
   var graph = new PackageGraph.forThisPackage();
   var phases = new PhaseGroup()
-    ..newPhase().addAction(new DeepEqualityBuilder(),
+    ..newPhase().addAction(new CompiledMirrorsBuilder(),
         new InputSet(graph.root.name, ['**/*.dart']));
 
   await build(phases);
