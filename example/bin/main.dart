646d7d4203e20d34ef49fbb0114ae6a8cf8b080b
Dave Shuckerow <djshuckerow@google.com>
2017-03-27 11:28:02 -0700

    Make main a little more clear about what goes on.

--- example/bin/main.dart
+++ example/bin/main.dart
@@ -3,8 +3,10 @@ import 'package:example/example.dart';
 void main() {
   var eq1 = new Eq1();
   var eq2 = new Eq1();
-  print(eq1 == eq2);
+  print('two eqs without their equals methods implemented will not be '
+      'equal: ${eq1 == eq2}');
   var eq1Mirror = new Eq1$CompiledMirror(eq1);
   var eq2Mirror = new Eq1$CompiledMirror(eq2);
-  print(eq1Mirror.deepEquals(eq2Mirror));
+  print('two mirrors can determine that they are equal: '
+      '${eq1Mirror.deepEquals(eq2Mirror)}');
 }
