ce494f94957e5b9cfab83dc338060f039c3c7992
Dave Shuckerow <djshuckerow@google.com>
2017-03-27 14:49:51 -0700

    Cleanup tests a bit.

--- test/transformer_test.dart
+++ test/transformer_test.dart
@@ -1,6 +1,5 @@
 import 'package:compiled_mirrors/src/transformer/builder.dart';
 import 'package:test/test.dart';
-import 'package:build_test/build_test.dart';
 
 import 'util.dart';
 
@@ -12,28 +11,19 @@ main() {
 
   group('Builder generates for a library', () {
     test('with a single class', () async {
-      var compiledMirrors = await compiledMirrorsSource;
-      await testBuilder(
-        builderToTest,
-        {
-          compiledMirrors.id: compiledMirrors.contents,
-          _oneClassAnnotatedLibrary.input.id:
-              _oneClassAnnotatedLibrary.input.contents,
-        },
-        generateFor: new Set.from([_oneClassAnnotatedLibrary.input.id]),
-        outputs: {
-          _oneClassAnnotatedLibrary.output.id:
-              _oneClassAnnotatedLibrary.output.contents,
-        },
-      );
+      await testBuilderWithAssets(builderToTest, [_oneClassAnnotatedLibrary]);
+    });
+    test('with varied type annotations', () async {
+      await testBuilderWithAssets(
+          builderToTest, [_varAndFinalAnnotatedLibrary]);
     });
   });
 }
 
 const TestAsset _oneClassAnnotatedLibrary = const TestAsset(
   const SourceAsset(
-      'test_builder|lib/one_class.dart',
-      r'''
+    'test_builder|lib/one_class.dart',
+    r'''
 import 'package:compiled_mirrors/compiled_mirrors.dart';
 
 @compileMirror
@@ -41,10 +31,11 @@ class Eq1 {
   String foo;
   int bar;
 }
-'''),
+''',
+  ),
   const SourceAsset(
-      'test_builder|lib/one_class.compiled_mirrors.dart',
-      r'''
+    'test_builder|lib/one_class.compiled_mirrors.dart',
+    r'''
 import 'package:compiled_mirrors/compiled_mirrors.dart';
 import 'one_class.dart';
 
@@ -63,5 +54,50 @@ class Eq1$CompiledMirror extends CompiledMirror<Eq1> {
     };
 
 }
-'''),
+''',
+  ),
+);
+
+const TestAsset _varAndFinalAnnotatedLibrary = const TestAsset(
+  const SourceAsset(
+    'test_builder|lib/varied_types.dart',
+    r'''
+import 'package:compiled_mirrors/compiled_mirrors.dart';
+
+@compileMirror
+class VariedTypes {
+  static const constantField = 'static const';
+  var foo = 7;
+  final int bar = 8;
+  final int baz;
+  var bop;
+}
+''',
+  ),
+  const SourceAsset(
+    'test_builder|lib/varied_types.compiled_mirrors.dart',
+    r'''
+import 'package:compiled_mirrors/compiled_mirrors.dart';
+import 'one_class.dart';
+
+class VariedTypes$CompiledMirror extends CompiledMirror<VariedTypes> {
+  @override
+  final VariedTypes instance;
+
+  @override
+  final Map<Symbol, FieldAccessor> fields;
+
+  VariedTypes$CompiledMirror(VariedTypes instance):
+    this.instance = instance,
+    this.fields = {
+      #constantField: () => instance.constantField;
+      #foo: () => instance.foo,
+      #bar: () => instance.bar,
+      #baz: () => instance.baz,
+      #bop: () => instance.bop,
+    };
+
+}
+''',
+  ),
 );
--- test/util.dart
+++ test/util.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:build/build.dart';
+import 'package:build_test/build_test.dart';
+
 class SourceAsset {
   final String id;
   final String contents;
@@ -15,12 +18,35 @@ class TestAsset {
   const TestAsset(this.input, this.output);
 }
 
+SourceAsset _compiledMirrorsSource;
 Future<SourceAsset> get compiledMirrorsSource async {
+  if (_compiledMirrorsSource != null) {
+    return _compiledMirrorsSource;
+  }
   var runfiles = Platform.environment['RUNFILES'];
   print(runfiles);
   var annotationFile = 'lib/compiled_mirrors.dart';
-  return new SourceAsset(
+  return _compiledMirrorsSource = new SourceAsset(
     'compiled_mirrors|lib/compiled_mirrors.dart',
     await new File('$annotationFile').readAsString(),
+  );
+}
+
+Future<Null> testBuilderWithAssets(
+    Builder toTest, Iterable<TestAsset> assets) async {
+  var compiledMirrors = await compiledMirrorsSource;
+  var inputs = <String, String>{
+    compiledMirrors.id: compiledMirrors.contents,
+  };
+  var outputs = <String, String>{};
+  for (var asset in assets) {
+    inputs[asset.input.id] = asset.input.contents;
+    outputs[asset.output.id] = asset.output.contents;
+  }
+  return await testBuilder(
+    toTest,
+    inputs,
+    generateFor: new Set.from(assets.map((a) => a.input.id)),
+    outputs: outputs,
   );
 }
