20da48e6ab24a43cba120bd852fae38bb3f0f3bc
Dave Shuckerow <djshuckerow@gmail.com>
2017-03-27 03:45:50 -0400

    Trying out the generation step.

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -1,5 +1,7 @@
 import 'package:compiled_mirrors/compiled_mirrors.dart';
 
+import 'example.compiled_mirrors.dart' as g;
+
 @compileMirror
 class Eq1 {
   String foo;
--- example/web/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:example/example.dart';
-
-void main() {
-  var eq1 = new Eq1();
-  var eq2 = new Eq1();
-  print(eq1 == eq2);
-}
